72d06adf87539c2180a8853a1e54b6e8190e59be
ryo-murai <murai.ry@gmail.com>
2013-09-02 16:49:19 +0900

    added proxomitron 4.5 june

---
+++ Proxomitron45J/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'Proxomitron'
+$url = 'http://www.proxomitron.info/files/download/ProxN45j.zip'
+$url64 = $url
+$validExitCodes = @(0)
+
+# download and unpack a zip file
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
