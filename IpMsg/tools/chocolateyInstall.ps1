9abfbff3b199e40afeab6d1a4434b511637f3278
ryo-murai <murai.ry@gmail.com>
2013-09-03 17:10:12 +0900

    added error handling

--- IpMsg/tools/chocolateyInstall.ps1
+++ IpMsg/tools/chocolateyInstall.ps1
@@ -1,10 +1,13 @@
-﻿$packageName = 'IpMsg'
-$installerType = 'exe'
-$url = 'http://ipmsg.org/archive/ipmsg342r2_installer.exe'
-$url64 = 'http://ipmsg.org/archive/ipmsg342r2_installer64.exe'
-$silentArgs = '' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is alw
-$validExitCodes = @(0)
+﻿try {
+  $packageName = 'IpMsg'
+  $installerType = 'exe'
+  $url = 'http://ipmsg.org/archive/ipmsg342r2_installer.exe'
+  $url64 = 'http://ipmsg.org/archive/ipmsg342r2_installer64.exe'
+  $silentArgs = '' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is alw
+  $validExitCodes = @(0)
 
-# download and unpack a zip file
-#Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"
+  Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
