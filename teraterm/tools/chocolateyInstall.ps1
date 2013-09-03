9d084f99f6ec5d7ff193dfc74652baf6f510af28
ryo-murai <murai.ry@gmail.com>
2013-09-03 19:21:44 +0900

    added teraterm package

---
+++ teraterm/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'teraterm' 
+$url = 'http://sourceforge.jp/frs/redir.php?m=iij&f=/ttssh2/59442/teraterm-4.79.zip'
+$validExitCodes = @(0)
+
+# download and unpack a zip file
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
