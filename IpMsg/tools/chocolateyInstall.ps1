cd5037e45cc9870c725eb76a612d12b13a08ca98
ryo-murai <murai.ry@gmail.com>
2013-09-03 15:55:19 +0900

    added IpMsg

---
+++ IpMsg/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'IpMsg'
+$installerType = 'exe'
+$url = 'http://ipmsg.org/archive/ipmsg342r2_installer.exe'
+$url64 = 'http://ipmsg.org/archive/ipmsg342r2_installer64.exe'
+$silentArgs = '' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is alw
+$validExitCodes = @(0)
+
+# download and unpack a zip file
+#Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"
