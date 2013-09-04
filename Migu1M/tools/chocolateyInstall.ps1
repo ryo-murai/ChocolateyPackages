ed4a8955c917d98b762cf7b7beb01c44665f3db2
ryo-murai <murai.ry@gmail.com>
2013-09-04 16:55:21 +0900

    fixed a bug: unable to install the fonts

--- Migu1M/tools/chocolateyInstall.ps1
+++ Migu1M/tools/chocolateyInstall.ps1
@@ -3,26 +3,32 @@
 #
 
 try {
-  $package = 'Migu1M'
-  $fontUrl = 'http://sourceforge.jp/projects/mix-mplus-ipa/downloads/59021/migmix-1m-20130617.zip/'
-  $destination = Join-Path $Env:Temp $package
+  $packageName = 'Migu1M'
+  $fontUrl = 'http://sourceforge.jp/frs/redir.php?m=iij&f=%2Fmix-mplus-ipa%2F59021%2Fmigmix-1m-20130617.zip'
+  $destination = Join-Path $Env:Temp $packageName
 
-  Install-ChocolateyZipPackage -url $fontUrl -unzipLocation $destination
+  $tempDir = Join-Path $env:TEMP "chocolatey" | Join-Path -ChildPath "$packageName"
+  if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+  $file = Join-Path $tempDir "$($packageName)Install.zip"
+
+  Get-ChocolateyWebFile $packageName $file $fontUrl
+  Get-ChocolateyUnzip "$file" $destination $specificFolder $packageName
 
   $FONTS = 0x14  # magic number
   $shell = New-Object -ComObject Shell.Application
   $fontsFolder = $shell.Namespace($FONTS)
 
-  $fontFiles = Get-ChildItem $destination -Recurse -Filter *.ttf
+  $fontFiles = Get-ChildItem $destination -Recurse -Filter *.ttf | % { $_.FullName }
 
   foreach($fontFile in $fontFiles) {
+    Write-Debug "installing $($fontFile) to $($fontsFolder)"
     $fontsFolder.CopyHere($fontFile)
   }
 
   Remove-Item $destination -Recurse
 
-  Write-ChocolateySuccess $package
+  Write-ChocolateySuccess $packageName
 } catch {
-  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
   throw
 }
