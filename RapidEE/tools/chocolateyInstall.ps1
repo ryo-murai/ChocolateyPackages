02cd4ab6eb90a02f5c784bea28929d5eedd6efd8
ryo-murai <murai.ry@gmail.com>
2013-09-11 11:09:48 +0900

    added rapid environment editor package

---
+++ RapidEE/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = 'RapidEE'
+$installerType = 'exe'
+
+$ver = '904'
+$url = "http://www.rapidee.com/download/archive/$ver/RapidEE_setup.exe"
+$url64 = $url
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+# main helpers - these have error handling tucked into them already
+# installer, will assert administrative rights
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
