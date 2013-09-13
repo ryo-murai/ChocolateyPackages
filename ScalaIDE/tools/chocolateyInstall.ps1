d04b1c8bd677ca5a88bb23323f509c1eb9cef535
ryo-murai <murai.ry@gmail.com>
2013-09-13 10:04:20 +0900

    added ScalaIDE package

---
+++ ScalaIDE/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'ScalaIDE'
+
+# http://scala-ide.org/download/sdk.html
+$url =   "http://downloads.typesafe.com/scalaide-pack/3.0.1.vfinal-210-20130718/scala-SDK-3.0.1-vfinal-2.10-win32.win32.x86.zip"
+$url64 = "http://downloads.typesafe.com/scalaide-pack/3.0.1.vfinal-210-20130718/scala-SDK-3.0.1-vfinal-2.10-win32.win32.x86_64.zip"
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
