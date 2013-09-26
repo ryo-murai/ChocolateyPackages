09bacd62825b52a67e71a66f1d560721073ee7d4
ryo-murai <murai.ry@gmail.com>
2013-09-26 17:04:10 +0900

    added Dexpot-portable package

---
+++ Dexpot-portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'Dexpot-portable'
+$url = 'http://www.dexpot.de/download/dexpot_1610_portable_r2373.zip'
+
+# download and unpack a zip file
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
