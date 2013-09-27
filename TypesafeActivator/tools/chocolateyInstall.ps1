bc741aca044f6ae2e5637373935125d28f85294f
ryo-murai <murai.ry@gmail.com>
2013-09-27 16:00:43 +0900

    added Typesafe Activator package

---
+++ TypesafeActivator/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'TypesafeActivator'
+$version = "1.0.0"
+$url = "http://downloads.typesafe.com/typesafe-activator/$version/typesafe-activator-$version.zip"
+
+# download and unpack a zip file
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
